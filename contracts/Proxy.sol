5bc9b650fb492b03693994c7138c5968edb8193b
ajlopez <webmaster@ajlopez.com>
2019-08-24 16:26:19 -0300

    Owner whitelisted in Proxy

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,7 +1,15 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract Proxy {
+    address owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
     function forward(address to, uint value, bytes memory data) public {
+        require(msg.sender == owner);
+        
         assembly {
             pop(call(gas, to, value, add(data, 0x20), mload(data), 0, 0))
         }
--- test/Proxy_test.js
+++ test/Proxy_test.js
@@ -2,6 +2,8 @@
 const Proxy = artifacts.require('./Proxy.sol');
 const Counter = artifacts.require('./Counter.sol');
 
+const expectThrow = require('./utils').expectThrow;
+
 const incrementHash = '0xd09de08a';
 
 contract('Proxy', function (accounts) {
@@ -18,6 +20,16 @@ contract('Proxy', function (accounts) {
         const result = (await this.counter.counter()).toNumber();
         
         assert.equal(result, 1);
+    });
+    
+    it('only whitelisted account could invoke forward', async function () {
+        const data = incrementHash;
+        
+        await expectThrow(this.proxy.forward(this.counter.address, 0, data, { from: accounts[1] }));
+        
+        const result = (await this.counter.counter()).toNumber();
+        
+        assert.equal(result, 0);
     });
 });
 
