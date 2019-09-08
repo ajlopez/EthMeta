e39d6a409d0dc10342ef5e35a774419422ffbf30
ajlopez <webmaster@ajlopez.com>
2019-09-08 14:51:52 -0300

    ProxyManager tests

--- test/ProxyManager_test.js
+++ test/ProxyManager_test.js
@@ -1,47 +1,50 @@
 
-const ProxyCreator = artifacts.require('./ProxyCreator.sol');
+const ProxyManager = artifacts.require('./ProxyManager.sol');
 const Proxy = artifacts.require('./Proxy');
 
 const expectThrow = require('./utils').expectThrow;
 
-contract('ProxyCreator', function (accounts) {
+contract('ProxyManager', function (accounts) {
+    const alice = accounts[0];
+    const bob = accounts[1];
+    
     beforeEach(async function () {
-        this.creator = await ProxyCreator.new();
+        this.manager = await ProxyManager.new();
     });    
    
    it('unknown proxy', async function () {
-        const result = await this.creator.proxies('0x01');
+        const result = await this.manager.proxies(bob);
         
         assert.equal(parseInt(result), 0);
     });
     
     it('create proxy', async function () {
-        await this.creator.createProxy('0x01');
+        await this.manager.createProxy(bob);
         
-        const result = await this.creator.proxies('0x01');
+        const result = await this.manager.proxies(bob);
         
         assert.ok(parseInt(result) != 0);
         
         const proxy = await Proxy.at(result);
         
-        const whitelisted = await proxy.whitelist(accounts[0]);
+        const user = await proxy.user();
         
-        assert.ok(whitelisted);
+        assert.equal(user, bob);
     });
     
-    it('cannot create proxy twice for the same hash', async function () {
-        await this.creator.createProxy('0x01');
-        await expectThrow(this.creator.createProxy('0x01'));
+    it('cannot create proxy twice for the same user', async function () {
+        await this.manager.createProxy(bob);
+        await expectThrow(this.manager.createProxy(bob));
         
-        const result = await this.creator.proxies('0x01');
+        const result = await this.manager.proxies(bob);
         
         assert.ok(parseInt(result) != 0);
         
         const proxy = await Proxy.at(result);
         
-        const whitelisted = await proxy.whitelist(accounts[0]);
+        const user = await proxy.user();
         
-        assert.ok(whitelisted);
+        assert.equal(user, bob);
     });
 });
 
