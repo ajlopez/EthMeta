995189ffdc97f1def3f2ff3cb8ed3cde8a252cd1
ajlopez <webmaster@ajlopez.com>
2019-09-08 16:27:39 -0300

    Updated command getproxy

--- commands/getproxy.js
+++ commands/getproxy.js
@@ -1,5 +1,6 @@
 
 const rskapi = require('rskapi');
+const simpleabi = require('simpleabi');
 const txs = require('./lib/txs');
 const keccak256 = require('./lib/sha3').keccak_256;
 
@@ -8,14 +9,14 @@ const config = require('./config.json');
 const host = rskapi.host(config.host);
 
 const user = process.argv[2];
-const hash = keccak256(user);
+const address = config.users[user].address;
 
 const tx = {
-    to: config.contracts.proxyCreator,
+    to: config.contracts.proxyManager,
     value: 0,
     gas: config.options && config.options.gas != null ? config.options.gas : 5000000,
     gasPrice: config.options && config.options.gasPrice != null ? config.options.gasPrice :60000000,
-    data: '0x' + keccak256('proxies(bytes32)').substring(0, 8) + hash
+    data: '0x' + keccak256('proxies(address)').substring(0, 8) + simpleabi.encodeValue(address)
 };
 
 (async function() {
