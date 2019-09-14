33067d24c87a8d9c0144396a369f38c2eb3b6f9d
ajlopez <webmaster@ajlopez.com>
2019-09-14 16:37:48 -0300

    Invoke command can use an account as sender instead of a user and relayer

--- commands/invoke.js
+++ commands/invoke.js
@@ -16,21 +16,25 @@ let args = process.argv[5];
 
 (async function() {
     try {
-        args = simpleabi.encodeCall(fn, await commands.arguments(host, config, args));
-        
-        let proxy = await commands.call(host, config, 'proxyManager', 'proxies(address)', [ config.users[user].address ]);
-        proxy = '0x' + proxy.substring(proxy.length - 40);
-        console.log('proxy', proxy);
+        if (config.users[user]) {
+            args = simpleabi.encodeCall(fn, await commands.arguments(host, config, args));
+            
+            let proxy = await commands.call(host, config, 'proxyManager', 'proxies(address)', [ config.users[user].address ]);
+            proxy = '0x' + proxy.substring(proxy.length - 40);
+            console.log('proxy', proxy);
 
-        const msghash = await commands.call(host, config, proxy, 'getMessageHash(address,uint256,bytes)', [contract, 0, args]);
-        console.log('msghash', msghash);
-        
-        const signature = ethutils.ecsign(new Buffer(msghash.substring(2), 'hex'), new Buffer(config.users[user].privateKey.substring(2), 'hex'));
-        const rpcsig = ethutils.toRpcSig(signature.v, signature.r, signature.s);
-        
-        console.log('signature', rpcsig);
-        
-        await commands.invoke(host, config, 'root', proxy, 'forward(bytes,address,uint256,bytes)', [ rpcsig, contract, 0, args ]);
+            const msghash = await commands.call(host, config, proxy, 'getMessageHash(address,uint256,bytes)', [contract, 0, args]);
+            console.log('msghash', msghash);
+            
+            const signature = ethutils.ecsign(new Buffer(msghash.substring(2), 'hex'), new Buffer(config.users[user].privateKey.substring(2), 'hex'));
+            const rpcsig = ethutils.toRpcSig(signature.v, signature.r, signature.s);
+            
+            console.log('signature', rpcsig);
+            
+            await commands.invoke(host, config, 'root', proxy, 'forward(bytes,address,uint256,bytes)', [ rpcsig, contract, 0, args ]);
+        }
+        else
+            await commands.invoke(host, config, user, contract, fn, args);
     }
     catch (ex) {
         console.log(ex);
